ef957408bee47e90ea5d6412b2043fc37360098c
Akshita Panigrahi <106181017+akshitapanigrahi@users.noreply.github.com>
2024-07-01 16:36:34 -0400

    Import bossdb_to_tiff_converter as package in test_image_conversion.py

--- tests/test_image_conversion.py
+++ tests/test_image_conversion.py
@@ -2,7 +2,7 @@ import pytest
 import os
 import numpy as np
 from unittest.mock import patch, MagicMock
-from bossdb_to_tiff_converter import get_indices, intern_convert, cloud_convert
+from .. import bossdb_to_tiff_converter
 
 # Constants for testing
 URL = "wildenberg2023/mouse_v1_l4_p105/em"
@@ -36,22 +36,22 @@ def cloud_dataset_mock():
 def test_get_indices_invalid_range():
     coord_dims = {'X': 10, 'Y': 10, 'Z': 10}
     with pytest.raises(ValueError):
-        get_indices('X', coord_dims, '5:15')
+        bossdb_to_tiff_converter.get_indices('X', coord_dims, '5:15')
     with pytest.raises(ValueError):
-        get_indices('Y', coord_dims, '10:5')
+        bossdb_to_tiff_converter.get_indices('Y', coord_dims, '10:5')
 
 # Test intern_convert
-@patch('bossdb_to_tiff_converter.array')
+@patch('bossdb_image_conversion.bossdb_to_tiff_converter.array')
 def test_intern_convert(mock_array, intern_dataset_mock, tmpdir):
     mock_array.return_value = intern_dataset_mock
     file_path = str(tmpdir.mkdir("test_output"))
-    intern_convert(f"bossdb://{URL}", URL.replace("/", "_"), RESOLUTION, X_DIMENSIONS, Y_DIMENSIONS, Z_DIMENSIONS, file_path)
+    bossdb_to_tiff_converter.intern_convert(f"bossdb://{URL}", URL.replace("/", "_"), RESOLUTION, X_DIMENSIONS, Y_DIMENSIONS, Z_DIMENSIONS, file_path)
     assert len(os.listdir(file_path)) == 100
 
 # Test cloud_convert
-@patch('bossdb_to_tiff_converter.CloudVolume')
+@patch('bossdb_image_conversion.bossdb_to_tiff_converter.CloudVolume')
 def test_cloud_convert(mock_cloudvolume, cloud_dataset_mock, tmpdir):
     mock_cloudvolume.return_value = cloud_dataset_mock
     file_path = str(tmpdir.mkdir("test_output"))
-    cloud_convert(f"s3://bossdb-open-data/{URL}", URL.replace("/", "_"), RESOLUTION, X_DIMENSIONS, Y_DIMENSIONS, Z_DIMENSIONS, file_path)
+    bossdb_to_tiff_converter.cloud_convert(f"s3://bossdb-open-data/{URL}", URL.replace("/", "_"), RESOLUTION, X_DIMENSIONS, Y_DIMENSIONS, Z_DIMENSIONS, file_path)
     assert len(os.listdir(file_path)) == 100
