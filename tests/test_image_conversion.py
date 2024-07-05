dc75a01bce8f59509d8ce01b7015c99532259777
Akshita Panigrahi <106181017+akshitapanigrahi@users.noreply.github.com>
2024-07-05 12:33:34 -0400

    Remove mock datasets/patching in test_image_conversion.py
    PyTests no longer use mock datasets nor patching for more convenient access of root directory

--- tests/test_image_conversion.py
+++ tests/test_image_conversion.py
@@ -1,7 +1,5 @@
 import pytest
 import os
-import numpy as np
-from unittest.mock import patch, MagicMock
 from .. import bossdb_to_tiff_converter
 
 # Constants for testing
@@ -12,27 +10,7 @@ Y_DIMENSIONS = "0:100"
 Z_DIMENSIONS = "0:100"
 FILE_PATH = "./test_output"
 
-# Fixtures
-@pytest.fixture
-def intern_dataset_mock():
-    data = np.random.randint(0, 255, (100, 100, 100), dtype=np.uint8)
-    mock_dataset = MagicMock()
-    mock_dataset.shape = data.shape
-    mock_dataset.__getitem__.side_effect = lambda key: data[key]
-    return mock_dataset
-
-@pytest.fixture
-def cloud_dataset_mock():
-    data = np.random.randint(0, 255, (100, 100, 100, 1), dtype=np.uint8)
-    mock_dataset = MagicMock()
-    mock_dataset.shape = data.shape
-    mock_dataset.__getitem__.side_effect = lambda key: data[key]
-    mock_dataset.bounds.minpt = [0, 0, 0]
-    mock_dataset.bounds.maxpt = [100, 100, 100]
-    mock_dataset.available_mips = [RESOLUTION]
-    return mock_dataset
-
-# Test exception handling
+# Test index exception handling
 def test_get_indices_invalid_range():
     coord_dims = {'X': 10, 'Y': 10, 'Z': 10}
     with pytest.raises(ValueError):
@@ -41,17 +19,14 @@ def test_get_indices_invalid_range():
         bossdb_to_tiff_converter.get_indices('Y', coord_dims, '10:5')
 
 # Test intern_convert
-@patch('bossdb_image_conversion.bossdb_to_tiff_converter.array')
-def test_intern_convert(mock_array, intern_dataset_mock, tmpdir):
-    mock_array.return_value = intern_dataset_mock
+def test_intern_convert(tmpdir):
+    #mock_array.return_value = intern_dataset_mock
     file_path = str(tmpdir.mkdir("test_output"))
     bossdb_to_tiff_converter.intern_convert(f"bossdb://{URL}", URL.replace("/", "_"), RESOLUTION, X_DIMENSIONS, Y_DIMENSIONS, Z_DIMENSIONS, file_path)
     assert len(os.listdir(file_path)) == 100
 
 # Test cloud_convert
-@patch('bossdb_image_conversion.bossdb_to_tiff_converter.CloudVolume')
-def test_cloud_convert(mock_cloudvolume, cloud_dataset_mock, tmpdir):
-    mock_cloudvolume.return_value = cloud_dataset_mock
+def test_cloud_convert(tmpdir):
     file_path = str(tmpdir.mkdir("test_output"))
     bossdb_to_tiff_converter.cloud_convert(f"s3://bossdb-open-data/{URL}", URL.replace("/", "_"), RESOLUTION, X_DIMENSIONS, Y_DIMENSIONS, Z_DIMENSIONS, file_path)
     assert len(os.listdir(file_path)) == 100
